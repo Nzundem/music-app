cb0707ce8a0805b8949cb1f4d07f5af973f51966
Nzoundem Essien <nzundemessien18@gmail.com>
2020-09-25 05:34:37 +0100

    Updtae from music app

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 window.addEventListener('load',()=>{
     const sounds =  document.querySelectorAll('.sounds');
     const pads =  document.querySelectorAll('.pads div');
+    const visuals = document.querySelector('.visual');
+    const colors = [
+        "#60d394",
+        "#d36060",
+        "#c060d3",
+        "#d3d160",
+        "#606bd3",
+        "#60c2d3"
+    ]
     console.log(sounds)
 
 //Lets get going with th sound
@@ -8,6 +17,18 @@ window.addEventListener('load',()=>{
         pad.addEventListener('click',function(){
             sounds[index].currentTime = 0
             sounds[index].play();
+            createBubbles(index)
         })
     })
+    //Create function to make bubble when pad is clicked
+    const createBubbles = (index)=>{
+        const bubble = document.createElement('div');
+        visuals.appendChild(bubble);
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = "jump 1s ease"
+        bubble.addEventListener('animationend',function(){
+            visuals.removeChild(this)
+        })
+    }
+    
 })
